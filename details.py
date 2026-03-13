531f03ec48a21ba943f61dcc8a03ba41aa95253b
Miawright123 <miawright115@gmail.com>
2026-03-13 13:22:57 +0200

    auto-graded task 1 (task 3)

---
+++ details.py
@@ -0,0 +1,13 @@
+#Get details from user using input
+
+name = input("Enter your name:")
+
+age = input("Enter your age:")
+
+house_number = input("Enter your house number:")
+
+street_name = input("Enter your street name:")
+
+#print a single sentence with these user details
+
+print(f"This is {name}. He is {age} years old and lives in house number {house_number} on {street_name}.")
